06077ba0a3c9cc8a81271725a406839d6269a00a
Harsh <harsh.26020@gmail.com>
2020-09-02 20:03:36 +0530

    add vertex-coloring

--- vertex-coloring.cpp
+++ vertex-coloring.cpp
@@ -1,6 +1,10 @@
-#include <stdio.h>
-#include <mpi.h>
+// Luby Jones algorithm
 
+#include <stdio.h>
+#include <stdlib.h> 
+#include <mpi.h>
+#include <time.h> 
+#include <set>
 using namespace std;
 
 int main()
@@ -10,16 +14,18 @@ int main()
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
+
 	int graph[101][101];
 	int color[101];
+	int weight[101];
 	int N, M;
-	
-	memset(graph, 0,sizeof(graph));
-	memset(color, -1,sizeof(color));
+
+	memset(graph, 0, sizeof(graph));
+	memset(color, -1, sizeof(color));
 
 	if(rank == 0)
 	{
+		
 		cin >> N >> M;
 		
 		for (int i = 0; i < m; ++i)
@@ -29,9 +35,163 @@ int main()
 			graph[v][u] = 1;
 		}
 
+		int max_deg = 0;
+		int cur_count = 0;
+		for (int i = 1; i <= N; ++i)
+		{
+			cur_count = 0;
+			for (int j = 1; j <= N; ++j)
+			{
+				cur_count += graph[i][j];
+			}
+			max_deg = max(max_deg, cur_count);
+		}
 
+		int max_col = max_deg + 1;
+		int vertex_per_proc = N / size;
+		int vertex_last_proc = N % size;
+		if(vertex_last_proc == 0)vertex_last_proc = vertex_per_proc;
 
+		int l = 1;
+
+		// random seed
+		srand(time(0)); 
+		
+		// Assign random weights
+		for (int i = 1; i <= N; ++i)
+		{
+			weight[i] = rand();
+		}
+
+		for (int i = 1; i <= size; ++i)
+		{
+			cur_count = vertex_per_proc;
+			if(i == size)cur_count = vertex_last_proc;
+			
+			// send number of vertices
+			MPI_Send(&N, 1, MPI_INT, i, i, MPI_COMM_WORLD);
+
+			// send randomly assigned weights
+			MPI_Send(&weight, N + 1, MPI_INT, i, i, MPI_COMM_WORLD);
+
+			// send graph
+			for(int j = 1; j <= N; ++j)
+			{
+				MPI_Send(&graph[j], N + 1, MPI_INT, i, i, MPI_COMM_WORLD);
+			}
+
+			int data[3] = {l, l + cur_count - 1, max_col};
+			// send range of vertices and max allowed color
+			MPI_Send(&data, 3, MPI_INT, i, i, MPI_COMM_WORLD);
+
+			l += cur_count;
+		}
+
+		// receive color from all processes
+		for(int i = 1; i <= size; i++)
+		{
+			MPI_Recv(color, N, MPI_INT, i, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+
+		max_col = 0;
+		for (int i = 1; i <= N; ++i)
+		{
+			assert(color[i] != -1);
+			max_col = max(max_col, color[i]);
+		}
+
+		cout << max_col << "\n";
+		for (int i = 1; i <= N; ++i)
+		{
+			cout << color[i] << " ";
+		}
+		cout << "\n";
 	}
+	else
+	{
+		// receive number of vertices
+		MPI_Recv(&N, 1, MPI_INT, 0, rank, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
+		// receive randomly assigned weights
+		MPI_Recv(&weight, N + 1, 0, rank, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		// receive graph
+		for(int i = 1; i <= N; ++i)
+		{
+			MPI_Recv(&graph[i], N + 1, 0, rank, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+
+		int data[3];
+
+		// receive range of vertices and max allowed color
+		MPI_Recv(&data, 3, 0, rank, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+
+		int done = 0, assign = 0;
+		while(!done)
+		{
+			done = 1;
+			assign = 1;
+			for (int v = data[0]; v <= data[1]; ++v)
+			{
+				if(color[v] == -1)
+				{
+					done = 0;
+					for (int i = 1; i <= N; ++i)
+					{
+						if(graph[v][i] && color[i] != -1 && weight[i] > weight[v])
+						{
+							assign = 0;
+						}
+					}
+
+				}
+			}
+			if(assign)
+			{
+				set<int>forbidden;
+				for (int i = 1; i <= N; ++i)
+				{
+					if(graph[v][i] && color[i] != -1)
+					{
+						forbidden.insert(color[i]);
+					}
+				}
+				for (int i = 1; i <= data[2]; ++i)
+				{
+					if(forbidden.find(i) == forbidden.end())
+					{
+						color[v] = i;
+						break;
+					}
+				}
+			}
+			for (int i = 1; i <= size; ++i)
+			{
+				if(i != rank)
+				{
+					MPI_Send(&color, N + 1, MPI_INT, i, i, MPI_COMM_WORLD);
+				}
+			}
+			for (int i = 1; i <= size; ++i)
+			{
+				if(i != rank)
+				{
+					int recv_col[101];
+					MPI_Recv(&recv_col, N + 1, i, rank, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+					for (int j = 1; j <= N; ++j)
+					{
+						if(color[j] == -1 && recv_col[j] != -1)
+						{
+							color[j] = recv_col[j];
+						}
+					}
+				}
+			}
+		}
+		MPI_Send(color, N + 1, MPI_INT, 0, rank, MPI_COMM_WORLD);
+	}
+	MPI_Finalize();
 	return 0;
 }
